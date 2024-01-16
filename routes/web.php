af33fda6aac972daeff66ae43385451a55c61675
Kunanont Rungrujee <aunsamyan170@gmail.com>
2024-01-16 07:36:36 +0700

    Making AdminLTE form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\MultiplyController;
+use App\Http\Controllers\C_titles;
+use App\Http\Controllers\MyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,18 +16,36 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::resource('titles', C_titles::class);
+
+Route::get('/my-controller', [MyController::class, 'index']);
+
+Route::get('/my-controller2', 'App\Http\Controllers\MyController@index');
+Route::namespace('App\Http\Controllers')->group(function(){
+    Route::get('/my-controller3', 'MyController@index');
+    Route::post('/my-controller3-post', 'MyController@store');
+});
+
+Route::resource('/my-controller4', MyController::class);
+
+
 Route::get('/', function () {
     return view('welcome'); // welcome.blade.php
 });
 
-Route::get('/multiple', function(){
-    return view('multiple');
+// use Illuminate\Http\Request;
+
+Route::get('/my-route', function(){
+    // return view('myroute');
+    //        Key    =>  Value
+    $data = ['val_a' => 'Hello World!'];
+    $data['val_b'] = "Laravel";
+    return view('myfolder.mypage',$data);
 });
 
 
-Route::post('/multiple', function(Request $req){
+Route::post('/my-route', function(Request $req){
     $data['myinput'] =  $req->input('myinput');
-    return view('multiple', $data);
+    return view('myroute', $data);
 });
-
-Route::post('/result', [MultiplyController::class, 'multiply']);
